4ccb6cac21309a717d883fe41ecaf73181f71b37
centrino.kib@gmail.com <centrino.kib@gmail.com>
2023-12-28 13:15:13 +0000

    feat: send props from calendar to event through parent app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import './App.css';
-import React  from 'react';
+import React, { useState }  from 'react';
 import { Calendar } from './components/Calendar';
+import { Event } from './components/Event';
 
 function App() {
+
+  const [selectedDate, setSelectedDate] = useState( new Date());
+  const [selectedDay, setSelectedDay] = useState( (new Date()).getDate());
+  const [selectedDayOfWeek, setSelectedDayOfWeek] = useState( (new Date()).getDay());
+
+  
+  const handleSelectedDate = ({day, dayOfWeek, date }) => {
+    setSelectedDate(date);
+    setSelectedDay(day);
+    setSelectedDayOfWeek(dayOfWeek);
+  }
+
+
+  console.log('handleSelectedDate:', handleSelectedDate);
+
   return (
   
       <div className='App' >
         <h1> eCalendar</h1>  
-        <Calendar/>
+        <Calendar onDateSelect={handleSelectedDate}/>
+        <Event 
+          date={selectedDate} 
+          day={selectedDay}
+          dayOfWeek={selectedDayOfWeek}
+        />
+        {/* <Event/> */}
       </div>
     
   );
--- src/components/Event.js
+++ src/components/Event.js
@@ -1,10 +1,20 @@
 
+import React from "react";
 import { useEffect, useState } from "react"
+import { solid } from '@fortawesome/fontawesome-svg-core/import.macro'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-export const Event = ({date, dayOfWeek }) => {
+
+
+export const Event = ({day, dayOfWeek, date}) => 
+{
+
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
 
     const [eventList, setEvents] = useState([]);
 
+    console.log("date is ", date);
 
     const singleEvent = {
         title: "",
@@ -17,32 +27,50 @@ export const Event = ({date, dayOfWeek }) => {
         description: 0,
     }
 
-    useEffect = ( () => {
+    // useEffect = ( () => {
+    //     console.log("day came through here: ", day);
+    //     console.log("date came through here: ", date);
 
-
-    }, [])
+    // }, [day, date])
 
 
     const displayEvents = () => {
+        console.log("date is ", date);
+        console.log("day is ", day);
+        return eventList.filter((event) => event.startDate.toDateString() === date.toDateString());
 
-        return eventList.filter( (event) => event.startDate === date)
+        // return eventList.filter( (event) => event.startDate === date)
+
+    }
+
+    const addEvent = () => {
+        console.log("date is ", date);
+        console.log("day is ", day);
+        
+    }
+
+    const dateConversion = (date) => {
+        let tempDate = date.toLocaleDateString(undefined, { month: 'long', day: 'numeric' }); 
+        return tempDate.split(" ")[1] + tempDate.split(" ")[0];
 
     }
 
     return (
 
-            <div>
-                {dayOfWeek}
-                {date.getMonth()} {date.getDate()}
-
+        <div>
+                <h1>Events Section</h1>
+                {daysOfWeek[dayOfWeek]}
+        
+                {dateConversion(date)}
                 <div className="eventsBox">
-                    {displayEvents.map( (event) => {
-                        <div> {event.title}  {event.begins} </div>
-                    })}
-
+                    {displayEvents().map((event) => (
+                        <div key={event.title}>{event.title}</div>
+                    ))}
                 </div>
 
-
+                <FontAwesomeIcon icon={solid("plus")} size="xl" style={{color: "#bcbcbc",}} 
+                    onClick={addEvent}
+                />
             </div>
         
         )
